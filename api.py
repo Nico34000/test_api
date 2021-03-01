cf021c9ee502d9beb477a32b475582f66da40d76
Nico34000 <77361662+Nico34000@users.noreply.github.com>
2021-03-01 13:59:18 +0100

    Update api.py

--- api.py
+++ api.py
@@ -40,15 +40,15 @@ def by_country(country):
     app.logger.warning(f"Acces a la route: latest_by_country/")
     if country in country_list():
         app.logger.debug(f"route demande: latest_by_country/ {country}")
-        return (latest_by_country(country))
+        return jsonify(latest_by_country(country))
         app.logger.debug(f"Operation reussi: {country}")
     elif country.lower() == country:
         app.logger.debug(f"route demande: latest_by_country/ {country}")
-        return (latest_by_country(country.capitalize()))
+        return jsonify(latest_by_country(country.capitalize()))
         app.logger.debug(f"Operation reussi: {country}")
     else:
         app.logger.error(f"la route demande n'existe pas: {country}")
-       
+        return abort(404)
 
 
 @app.route('/average_by_year/<year>')
@@ -66,10 +66,10 @@ def average_for_year(year):
     app.logger.warning(f"Acces a la route")
     if year in year_list():
         app.logger.debug(f"route demande :/average_by_year/ {year}")
-        return (average_year(year))
+        return jsonify(average_year(year))
     else:
         app.logger.error(f"la route demande n'existe pas: {year}")   
-        
+        return abort(404)
 
 
 @app.route('/per_capita/<country>')
@@ -85,14 +85,15 @@ def per_capita(country):
     app.logger.warning(f"Acces a la route")    
     if country in country_list():
         app.logger.debug(f"Route demande: /per_capita{country}")
-        return (per_capi(country))
+        return jsonify(per_capi(country))
     elif country.lower() == country:
         app.logger.debug(f"route demande: /per_capita {country}")
-        return (per_capi(country.capitalize()))
+        return jsonify(per_capi(country.capitalize()))
     else:
         app.logger.error(f"La route demande n'existe pas: {country}")
-        
+        return abort(404)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
